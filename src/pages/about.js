0cde962eaac87914b32481aaa98f36ce21222b5c
wildeink <misterjoy@fastmail.com>
2019-09-24 14:42:22 -0400

    step one

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from "../components/header";
+
+export default () => (
+  <div>
+    <Header headerText="About Gatsby" />
+    <Header headerText="Yup" />
+    <p>Such wow. Very React</p>
+  </div>
+)
