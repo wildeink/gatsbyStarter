07b8faf61d5cc64af3bb297002ed6265bae04576
wildeink <misterjoy@fastmail.com>
2019-10-01 13:32:20 -0400

    added files query and output

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,16 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Pandas Site`,
+    title: `Pandas Eating Lots`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
